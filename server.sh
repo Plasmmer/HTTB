5785867d7939cc8f6dee367b34cc9714281294db
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-18 16:00:55 +0000

    Snapshot

--- server.sh
+++ server.sh
@@ -1,11 +1,7 @@
-#while true;
-  #do echo -e "HTTP/1.1 200 OK\n\n$(date)" \
-  #| nc -l -k -p 8080 -q 1;
-  #while IFS= read -r INPUT || [ "$INPUT" ]; do
-#    echo "Req line: $INPUT" >&2
-#done; 
-#done
-
-while IFS= read -r INPUT || [ "$INPUT" ]; do
+while true;
+  do echo -e "HTTP/1.1 200 OK\n\n$(date)" \
+  | nc -l -k -p 8080 -q 1;
+  while IFS= read -r INPUT || [ "$INPUT" ]; do
     echo "Req line: $INPUT" >&2
-done;
+done 
+done
