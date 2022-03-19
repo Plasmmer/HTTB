4c5e2f8e5ba8c6217d47b6a94565a0c1c49cd2ea
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-19 19:38:13 +0000

    Snapshot

---
+++ server-old.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+while true; do
+##  do echo -e "HTTP/1.1 200 OK\n\n$(date)" \
+##  do echo -e "HTTP/1.1 200 OK\n\n$(less <&0)" \
+##  do echo -e "HTTP/1.1 200 OK\n\n$(cat -)" \
+##  do echo -e "HTTP/1.1 200 OK\n\n$(cat - &)" \
+##  STDIN=$(cat - &)
+##  echo -e "HTTP/1.1 200 OK\n\n$STDIN" \
+#  echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
+#  | nc -l -k -p 8080 -q 1;
+#  nc -l 8080 | cat - | grep -o '{...\+}' \
+#  | nc -l -k -p 8080 -q 1;
+#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
+#  | nc -l -k -p 8080 -q 1;
+#  nc -l -k -p 8080 -q 1 & | cat - | grep -o '{...\+}';
+#  cat - | grep -o '{...\+}' < nc -l -k -p 8080 -q 1;
+#echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
+#  | nc -l -k -p 8080 -q 1 &
+#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1><p>$(date)</p><br><input type=\"submit\" value=\"Submit\">" \
+#  | nc -l -k -p 8080 -q 1;
+#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1><p>$(date)</p>"
+#  | nc -l -k -p 8080 -q 1;
+#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
+#  | nc -l -k -p 8080 -q 1 | cat - | grep -o '{...\+}';
+#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
+#  | nc -l -k -p 8080 -q 1 & | cat - | grep -o '{...\+}';
+#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
+#  | nc -l -k -p 8080 -q 1 | cat - | grep -o '{...\+}';
+#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
+#  | nc -l -k -p 8080 -q 1;
+#  nc -l -k -p 8080 -q 1 | cat - | grep -o '{...\+}';
+#ncat -l 0.0.0.0 8080 --sh-exec "bash newserver.sh"
+#  ncat -l --keep-open 0.0.0.0 8080 --sh-exec "bash newserver.sh"
+  ncat -l --keep-open 0.0.0.0 8080 --sh-exec "bash server.sh"
+done
--- server.sh
+++ server.sh
@@ -1,35 +1,11 @@
+#!/usr/bin/bash
+declare -r DEBUG=1
 
-#!/bin/bash
+log() {
+    if [ $DEBUG = 1 ]
+    then
+        echo "$1" >&2;
+    fi
+}
 
-while true; do
-##  do echo -e "HTTP/1.1 200 OK\n\n$(date)" \
-##  do echo -e "HTTP/1.1 200 OK\n\n$(less <&0)" \
-##  do echo -e "HTTP/1.1 200 OK\n\n$(cat -)" \
-##  do echo -e "HTTP/1.1 200 OK\n\n$(cat - &)" \
-##  STDIN=$(cat - &)
-##  echo -e "HTTP/1.1 200 OK\n\n$STDIN" \
-#  echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
-#  | nc -l -k -p 8080 -q 1;
-#  nc -l 8080 | cat - | grep -o '{...\+}' \
-#  | nc -l -k -p 8080 -q 1;
-#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
-#  | nc -l -k -p 8080 -q 1;
-#  nc -l -k -p 8080 -q 1 & | cat - | grep -o '{...\+}';
-#  cat - | grep -o '{...\+}' < nc -l -k -p 8080 -q 1;
-#echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
-#  | nc -l -k -p 8080 -q 1 &
-#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1><p>$(date)</p><br><input type=\"submit\" value=\"Submit\">" \
-#  | nc -l -k -p 8080 -q 1;
-#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1><p>$(date)</p>"
-#  | nc -l -k -p 8080 -q 1;
-#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
-#  | nc -l -k -p 8080 -q 1 | cat - | grep -o '{...\+}';
-#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
-#  | nc -l -k -p 8080 -q 1 & | cat - | grep -o '{...\+}';
-#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
-#  | nc -l -k -p 8080 -q 1 | cat - | grep -o '{...\+}';
-#   echo -e "HTTP/1.1 200 OK\n\n<h1>Title</h1>$(date)" \
-#  | nc -l -k -p 8080 -q 1;
-#  nc -l -k -p 8080 -q 1 | cat - | grep -o '{...\+}';
-ncat -l 0.0.0.0 8080 --sh-exec "bash newserver.sh"
-done
+echo "<h1>Hello, World!</h1><p>It's like the early times of the Internet... ...but we are building the Web 3.0, actually.</p>"
