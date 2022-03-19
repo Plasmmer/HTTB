2f1018800ad232aa3592d9cf876d8e523f50b725
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-19 20:13:01 +0000

    Snapshot

--- server.sh
+++ server.sh
@@ -8,4 +8,17 @@ log() {
     fi
 }
 
-echo "<h1>Hello, World!</h1><p>It's like the early times of the Internet... ...but we are building the Web 3.0, actually.</p>"
+serve() {
+#    nc --listen --keep-open 0.0.0.0 7999 --sh-exec "./server.sh process"
+#    ncat --listen --keep-open 0.0.0.0 7999 --sh-exec "./server.sh process"
+    ncat --listen --keep-open 0.0.0.0 8080 --sh-exec "bash server.sh process"
+}
+
+process() {
+#    echo "<h1>Hello, World!</h1><p>It's like the early times of the Internet... ...but we are building the Web 3.0, actually.</p>"
+#    echo "<html><head></head><body><h1>Hello, World!</h1><p>It's like the early times of the Internet... ...but we are building the Web 3.0, actually.</p></body></html>"
+    echo -e "<html><head></head><body><h1>Hello, World!</h1><p>It's like the early times of the Internet... ...but we are building the Web 3.0, actually.</p></body></html>"
+    log "Hello, Error"
+}
+
+"$1"
