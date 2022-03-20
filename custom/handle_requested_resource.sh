78136e2d13dc954bf5ef128b963114ee8b8a920f
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-19 22:32:16 -0300

    Fix commit 27e81a03b6d0d887df651b03894c9ffd18c8be92 (Support custom hooks, starting by "handle_requested_resource")

--- custom/handle_requested_resource.sh
+++ custom/handle_requested_resource.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 log "$request_body"
-if jq -e . >/dev/null 2>&1 <<<"$json_string"; then
+if jq -e . >/dev/null 2>&1 <<<"$json_string"
+then
     echo "Parsed JSON successfully and got something other than false/null"
     json_username="$(echo "$request_body" | jq -r '.name')"
     json_age="$(echo "$request_body" | jq -r '.age')"
