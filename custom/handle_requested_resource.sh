4340392faea2f882774180d961f04a79b2999dc9
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-19 23:25:27 -0300

    Fix handle_requested_resource.sh
    It was echoing instead of simply logging

--- custom/handle_requested_resource.sh
+++ custom/handle_requested_resource.sh
@@ -3,10 +3,10 @@
 log "$request_body"
 if jq -e . >/dev/null 2>&1 <<<"$json_string"
 then
-    echo "Parsed JSON successfully and got something other than false/null"
+    log "Parsed JSON successfully and got something other than false/null"
     json_username="$(echo "$request_body" | jq -r '.name')"
     json_age="$(echo "$request_body" | jq -r '.age')"
     log "Name: $json_username | Age: $json_age"
 else
-    echo "Failed to parse JSON, or got false/null"
+    log "Failed to parse JSON, or got false/null"
 fi
