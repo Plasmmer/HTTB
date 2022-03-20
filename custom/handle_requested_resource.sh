27e81a03b6d0d887df651b03894c9ffd18c8be92
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-19 21:53:40 -0300

    Attempt to fix previous commit

--- custom/handle_requested_resource.sh
+++ custom/handle_requested_resource.sh
@@ -1,9 +1,11 @@
+#!/bin/sh
+
 log "$request_body"
 if jq -e . >/dev/null 2>&1 <<<"$json_string"; then
-#    echo "Parsed JSON successfully and got something other than false/null"
+    echo "Parsed JSON successfully and got something other than false/null"
     json_username="$(echo "$request_body" | jq -r '.name')"
     json_age="$(echo "$request_body" | jq -r '.age')"
     log "Name: $json_username | Age: $json_age"
 else
-#    echo "Failed to parse JSON, or got false/null"
+    echo "Failed to parse JSON, or got false/null"
 fi
--- test.sh
+++
@@ -1,9 +0,0 @@
-#json_string="{\"name\":\"loremipsum\", \"age\":\"34\"}"
-json_string="text"
-
-if jq -e . >/dev/null 2>&1 <<<"$json_string"; then
-    echo "Parsed JSON successfully and got something other than false/null"
-else
-    echo "Failed to parse JSON, or got false/null"
-fi
-
