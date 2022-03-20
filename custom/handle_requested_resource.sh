f90cc5d0867e73ff1cd6da0e0ee6fec50f7a5d57
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-20 11:38:02 -0300

    Fix custom/handle_requested_resource.sh

--- custom/handle_requested_resource.sh
+++ custom/handle_requested_resource.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 log "$request_body"
-if jq -e . >/dev/null 2>&1 <<<"$json_string"
+if jq -e . >/dev/null 2>&1 <<<"$request_body"
 then
     log "Parsed JSON successfully and got something other than false/null"
     json_username="$(echo "$request_body" | jq -r '.name')"
